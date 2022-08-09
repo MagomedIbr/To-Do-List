d2053bb7637b4017bf93d10268bef0908726f510
MagomedIbr <magomedibragimov@yahoo.de>
2022-08-09 12:55:30 +0200

    Die Daten aus dem Local Storage in die HTML laden. Funktioniert noch nicht weil das DOM Element als null geladen wird????

--- script.js
+++ script.js
@@ -4,16 +4,58 @@ console.log("todolist");
 const form = document.querySelector("#itemForm");
 const message = document.querySelector("#message");
 const input = document.querySelector("#itemInput");
-const itemList = document.querySelector("#itemList");
+const itemList = document.querySelector("#itemsList");
 const filter = document.querySelector(".nav-item");
 
 //create an empty Item List
 let todoItems = [];
+console.log(itemList);
+console.log(form);
+console.log(input)
+//for each https://www.w3schools.com/jsref/jsref_forEach.asp
+//Für jeden Parameter wird eine Function genutzt
+const getList = function (todoItems){
+    itemList.innerHTML = ``;
+    if(todoItems.length >0){
+        todoItems.forEach((item)=> {
+            let liTag = `
+            <li class=" list-group-item d-flex justify-content-between align-items-center size25">
+            <span> ${item.name} </span> 
+             <span>
+
+              <a> <i class="bi bi-check-circle green"></i> </a>
+              <a> <i class="bi bi-pencil-square blue"></i> </a>
+              <a> <i class="bi bi-x-circle red"></i> </a>
+
+            </span>
+            
+          </li>`
+            itemList.insertAdjacentHTML("beforeend",liTag);
+        });
+    }
+}
+
+
+//Für Localstorage https://www.w3schools.com/jsref/prop_win_localstorage.asp
 
 
 //Local Store Methode
 const setLocalStorage = function(todoItems){
     localStorage.setItem("todoItems", JSON.stringify(todoItems));
+}
+
+//Items aus dem Localstorage bekommen
+const getLocalStorage = function(){
+    const todoStorage = localStorage.getItem("todoItems");
+    if(todoStorage=="undefined"|| todoStorage===null){
+        todoItems = [];
+    }
+    else{
+        todoItems = JSON.parse(todoStorage); //um von Stringify wieder zurück zu einem Objekt zu konvertieren
+    }
+    console.log("items", todoItems)
+    console.log(todoItems.length)
+    getList(todoItems);
 }
 
 //Submit funktioniert mit dem button ind er html form. 
@@ -32,7 +74,9 @@ document.addEventListener("DOMContentLoaded", ()=> {
             };
             todoItems.push(itemObj);
             setLocalStorage(todoItems);
+            
         }
         console.log(itemName);
-    })
+    });
+    getLocalStorage();
 })
